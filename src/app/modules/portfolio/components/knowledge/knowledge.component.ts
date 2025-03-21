059389cd6248c5921b369e002441049553f572e8
Joao <j_donizetti@outlook.com>
2025-03-21 10:59:08 -0300

    Criando o knowledge.component(conhecimentos) e fazendo um for nos icons.svg do component

--- src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-knowledge',
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './knowledge.component.scss'
 })
 export class KnowledgeComponent {
-
+  public arrayKnowledge = signal([
+    {
+      src: 'assets/icons/knowledge/html.svg',
+      alt: 'Icone de conhecimento em html5'
+    },
+    {
+      src: 'assets/icons/knowledge/css.svg',
+      alt: 'Icone de conhecimento em css3'
+    },
+    {
+      src: 'assets/icons/knowledge/javaScript.svg',
+      alt: 'Icone de conhecimento em JavaScript'
+    },
+    {
+      src: 'assets/icons/knowledge/angular.svg',
+      alt: 'Icone de conhecimento em angular'
+    },
+  ])
 }
