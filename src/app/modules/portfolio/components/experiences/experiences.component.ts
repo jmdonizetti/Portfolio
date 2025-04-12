b3d2623b2a09d09f9bf831df85ce93383e987e80
Joao <j_donizetti@outlook.com>
2025-04-12 13:21:35 -0300

    Melhorando Scss da pagina

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -14,7 +14,7 @@ export class ExperiencesComponent {
     {
       summary: {
         strong: "Controlador de acesso",
-        p: "Grupo Coneleste | Nov 2023 a Jun/2024"
+        p: "Grupo Coneleste | Nov/2023 à Jun/2024"
       },
       text: "<p>Controle de acesso e monitoramento em ambiente corporativo. </p>",
     },
@@ -22,7 +22,7 @@ export class ExperiencesComponent {
     {
       summary: {
         strong: "Líder de portaria remota",
-        p: "CM Soluções | Abr 2019 a jan/2023"
+        p: "CM Soluções | Abr/2019 à Jan/2023"
       },
       text: "<p>Responsável pelo controle de acesso e monitoramento de condomínios residenciais.</p> <p> Liderança de equipe e gestão de rotinas operacionais. </p>",
     },
@@ -30,7 +30,7 @@ export class ExperiencesComponent {
     {
       summary: {
         strong: "Operador de telemarketing",
-        p: "ATENTO BRASIL | Maio 2018 a Mai/2019"
+        p: "Atento Brasil | Mai/2018 à Mai/2019"
       },
       text: "<p>Operador de telemarketing ativo e receptivo, responsável pela retenção de clientes pós compra.</p>",
     },
@@ -38,7 +38,7 @@ export class ExperiencesComponent {
     {
       summary: {
         strong: "Auxiliar de logística",
-        p: "BATISTELA | jun 2016 a jnov/2017"
+        p: "Batistela | Jun/2016 à Nov/2017"
       },
       text: "<p>Auxiliar de logística, recebimento de mercadoria, estocagem, separação de carga e limpeza do local.</p>"
     },
